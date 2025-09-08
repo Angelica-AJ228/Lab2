786f87719d808ea62365fd3a3ed272c8488cc6d5
Angelica Aguilar <isaj44631@gmail.com>
2025-09-07 21:36:20 -0600

    Comprobacion del usuario y argumentos

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+if [ "id -u" -ne 0 ]; then
+	echo "Error: se debe utilizar root para la ejecucion del programa"
+	exit 1
+fi
 
-#Hola!
+if [ $# -ne 3 ]; then
+	echo "Uso: $0 usuario grupo ruta"
+	exit 1
+fi
+
+usu=$1
+grp=$2
+arch=$3
+
+if [ ! e "$arch" ]; then
+	echo "Error: el archivo no existe"
+	exit 1
+fi
+
+
+
