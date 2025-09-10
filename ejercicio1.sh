286c4fe40adb05fafe0d12d4217778cb18fa5b03
Angelica Aguilar <isaj44631@gmail.com>
2025-09-09 20:21:33 -0600

    Ejercicio 1 listo

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "id -u" -ne 0 ]; then
-	echo "Error: se debe utilizar root para la ejecucion del programa"
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Error: script solo ejecutable por root."
 	exit 1
 fi
 
 if [ $# -ne 3 ]; then
-	echo "Uso: $0 usuario grupo ruta"
+	echo "Uso: $0 usuario grupo ruta_archivo"
 	exit 1
 fi
 
@@ -14,10 +14,28 @@ usu=$1
 grp=$2
 arch=$3
 
-if [ ! e "$arch" ]; then
-	echo "Error: el archivo no existe"
+if [ ! -e "$arch" ]; then
+	echo "Error: El archivo $arch no existe."
 	exit 1
 fi
 
+if groups | grep -w "$grp"; then
+	echo "El grupo $grp ya existe."
+else
+	addgroup "$grp"
+	echo "Grupo $grp creado."
+fi
 
+if id "$usu"; then
+	echo "El usuario $usu ya existe. Se agregará al grupo $grp."
+	adduser "$usu" "$grp"
+else
+	adduser --ingroup "$grp" "$usu"
+	echo "Usuario $usu creado en el grupo $grp."
+fi
+
+chown "$usu:$grp" "$arch"
+chmod 740 "$arch"
+
+echo "El archivo $arch pertenece a $usu:$grp. El usuario $usu tiene todos los permisos, el grupo $grp solo de lectura. Importante: los demas usuarios no tienen ningun permiso."
 
