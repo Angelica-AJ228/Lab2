576c9244a605f250ddf38259a7ba78f337d999e0
Angelica Aguilar <isaj44631@gmail.com>
2025-09-10 23:29:40 -0600

    Gnuplot implementado, falta corregir que se agregue la linea en la grafica

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -6,7 +6,7 @@ if [ $# -lt 1 ]; then
 fi
 
 comando=$@
-log="monitor.log"
+log="consumo.log"
 
 rm -f $log
 echo "Tiempo,CPU(%),Memoria(%)" > $log
@@ -20,8 +20,24 @@ while ps -p $pid; do
 	tiempo=$(date +"%H:%M:%S")
 	memoria=$(ps -p $pid -o %cpu,%mem --no-headers)
 	echo "$tiempo,$memoria" >> $log
-	sleep 2
+	sleep 6
 done
+
+gnuplot <<-EOF
+
+	set datafile separator ","
+	set terminal png size 800,600
+	set output "$HOME/consumo.png"
+	set title "Memoria y consumo CPU"
+	set xlabel "Tiempo"
+	set ylabel "Uso, %"
+	set key outside
+	plot "$log" using 0:2 with lines title "CPU", \
+	     "$log" using 0:3 with lines title "Memoria"
+
+EOF
+
+echo "Imagen descargada en $HOME/consumo.png"
 
 
 
