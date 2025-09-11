0a552bcc0f6363c4bba99112f6214f865a0cf38d
Angelica Aguilar <isaj44631@gmail.com>
2025-09-11 10:56:01 -0600

    Ejercicio 3 listo, pero no funciona

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+dir="/home/angelica/lab2"
+log="/home/angelica/lab2/Lab2/historial.log"
+
+echo "Analizando..."
+echo "Terminado. Analisis registrado en $dir" > "$log"
+
+inotifywait -m -r create -e modify -e delete "$dir" | tee -a "$log"
 
