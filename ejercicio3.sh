e7054167b416c3d35c2def55068d7f300842230d
Angelica Aguilar <isaj44631@gmail.com>
2025-09-11 18:02:54 -0600

    Comprobacion de que la carpeta existe y si no, la crea para que funcione bien el historial.log del servicio

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-dir="/home/angelica/lab2"
+dir="/home/angelica/lab2/Lab2"
 log="/home/angelica/lab2/Lab2/historial.log"
+
+touch /home/angelica/lab2/Lab2/historial.log
+
+if [ ! -d "/home/angelica/lab2/Lab2" ]; then
+	echo "Error: carpeta no existe" >> /home/angelica/lab2/Lab2/historial.log
+	exit 1
+fi
 
 echo "Analizando..."
 echo "Terminado. Analisis registrado en $dir" > "$log"
